684451ceaef209ba797122eb6bb91eff2875a6e6
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-23 11:38:14 -0300

    Update 2_Tabela.py

--- pages/2_Tabela.py
+++ pages/2_Tabela.py
@@ -20,16 +20,22 @@ tabela_preco.index += 1
 tabela_litros = tabela.sort_values(by='Litros Exportados', ascending=False).reset_index(drop=True)
 tabela_litros.index += 1
 
-#função para mostrar em milhões com 3 casa decimais
-format_millions = lambda x: "{:.3f}M".format(x / 1000000)
+#função para mostrar em milhões com 3 casa decimais, ou outro formato se for muito pequeno
+def format_magnitude(x):
+    if abs(x) >= 0.1 * 1e6:
+        return "{:.3f}M".format(x / 1e6)
+    elif abs(x) >= 1e3:
+        return "{:.1f}K".format(x / 1e3)
+    else:
+        return "{:.0f}".format(x)
 
 #modificando valores na tabela de preço
-tabela_preco['Preço Dólar'] = tabela_preco['Preço Dólar'].apply(format_millions)
-tabela_preco['Litros Exportados'] = tabela_preco['Litros Exportados'].apply(format_millions)
+tabela_preco['Preço Dólar'] = tabela_preco['Preço Dólar'].apply(format_magnitude)
+tabela_preco['Litros Exportados'] = tabela_preco['Litros Exportados'].apply(format_magnitude)
 
 #modificando valores na tabela de litros
-tabela_litros['Preço Dólar'] = tabela_litros['Preço Dólar'].apply(format_millions)
-tabela_litros['Litros Exportados'] = tabela_litros['Litros Exportados'].apply(format_millions)
+tabela_litros['Preço Dólar'] = tabela_litros['Preço Dólar'].apply(format_magnitude)
+tabela_litros['Litros Exportados'] = tabela_litros['Litros Exportados'].apply(format_magnitude)
 
 # Mostrando com st.dataframe
 st.write('Tabela por Preço')
