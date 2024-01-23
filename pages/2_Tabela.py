812537b3bfe0c6addcb0e7db220a873e7010dcfe
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-23 11:22:27 -0300

    Update 2_Tabela.py

--- pages/2_Tabela.py
+++ pages/2_Tabela.py
@@ -6,5 +6,19 @@ st.title('Placeholder')
 url='https://raw.githubusercontent.com/Renan-Carneiro/TechExpVinho/main/ExpVinhoTratado.csv'
 df = pd.read_csv(url, index_col=0)
 
-st.write('Tabela')
-st.write(df)
+#agrupando pela soma de litros e preço
+tabela = df.groupby('País').agg({'Litros Exportados': 'sum', 'Preço Dólar': 'sum'}).reset_index()
+
+#dropando linhas onde ambos valores são 0
+tabela = tabela.loc[(tabela['Preço Dólar'] != 0) | (tabela['Litros Exportados'] != 0)]
+
+# por preço em ordem decrescente
+tabela_preco = tabela.sort_values(by='Preço Dólar', ascending=False)
+
+# por litros em ordem decrescente
+tabela_litros = tabela.sort_values(by='Litros Exportados', ascending=False)
+
+st.write('Tabela por Preço')
+st.write(tabela_preco)
+st.write('Tabela por Litros Exportados')
+st.write(tabela_litros)
