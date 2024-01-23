b39563f6f276022c78697f8ca6e0a2f35bd795ed
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-23 11:48:51 -0300

    Update 2_Tabela.py

--- pages/2_Tabela.py
+++ pages/2_Tabela.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Placeholder')
+st.title('Tabelas dos Dados de Exportação de Vinhos')
 
 url='https://raw.githubusercontent.com/Renan-Carneiro/TechExpVinho/main/ExpVinhoTratado.csv'
 df = pd.read_csv(url, index_col=0)
