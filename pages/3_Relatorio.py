b4c4b9a68abc0f57dc5e9b4d5a4339d9a241d5fe
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-23 01:26:52 -0300

    embed

--- pages/3_Relatorio.py
+++ pages/3_Relatorio.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title('Placeholder')
+# Embed the reveal.js presentation using an iframe
+iframe_code = <iframe src="https://slides.com/renanmoraes/minimal/embed" width="576" height="420" title="Minimal" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+# Render the iframe code
+st.markdown(iframe_code, unsafe_allow_html=True)
