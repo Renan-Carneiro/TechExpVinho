3adcd572a61a0bc50699acb89fc54ffa42607963
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-23 01:19:29 -0300

    Update 2_Tabela.py

--- pages/2_Tabela.py
+++ pages/2_Tabela.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Placeholder')
+
+url='https://raw.githubusercontent.com/Renan-Carneiro/TechExpVinho/main/ExpVinhoTratado.csv'
+df = pd.read_csv(url, index_col=0)
+
+st.write('Tabela')
+st.write(df)
