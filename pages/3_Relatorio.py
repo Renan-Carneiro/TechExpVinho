51d01b1d896bc6bd895e1e440fdbdca79cfffecf
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-28 21:17:09 -0300

    Update 3_Relatorio.py

--- pages/3_Relatorio.py
+++ pages/3_Relatorio.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Relatório:')
 # Embed the reveal.js presentation using an iframe
-iframe_code = '<iframe src="https://onedrive.live.com/embed?resid=1F95875ED93EE176%2196167&amp;authkey=!AJgTggXUq0HIwY0&amp;em=2&amp;wdAr=1.7777777777777777" width="1600px" height="900px" frameborder="0">Este é um apresentação do <a target="_blank" href="https://office.com">Microsoft Office</a> incorporado, da plataforma <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>'
+iframe_code = '<iframe src="https://onedrive.live.com/embed?resid=1F95875ED93EE176%2196167&amp;authkey=!AJgTggXUq0HIwY0&amp;em=2&amp;wdAr=1.7777777777777777" width="1360px" height="768px" frameborder="0">Este é um apresentação do <a target="_blank" href="https://office.com">Microsoft Office</a> incorporado, da plataforma <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>'
 
 # Render the iframe code
 st.markdown(iframe_code, unsafe_allow_html=True)
